512e0ae60b5d2884225d0056a04c9b9c353d91e1
SneauxGirl <heatherh@liyudev.com>
2026-01-20 00:29:27 -0800

    Added JS files and animations for console and footer - currently non-functional

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//Code Switcher animation
+document.addEventListener("DOMContentLoaded", function() {
+    const tabs = document.querySelectorAll(".code-container__tab");
+    const codeBlocks = document.querySelectorAll(".code-container__code");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", function() {
+            const language = this.getAttribute("data-language");
+
+            //Remove active class from ALL tabs and code blocks
+            tabs.forEach(t => t.classList.remove("code-container__tab--active"));
+            codeBlocks.forEach(c => c.classList.remove("code-container__code--active"))
+
+            //Add active class only to the clicked tab and corresponsing code block
+            this.classList.add("code-container__tab--active");
+            document.querySelector(`.code-container__code--${language}`).classList.add("code-container__code--active");
+
+            //Re-highlight the code block for Theme - make sure this is compatible with my edits
+            PerformanceServerTiming.highlightElement(document.querySelector(`.code-container__code--${language} code`));
+        });
+    })
+})
+
+//Footer animation
+const footer = document.querySelector('footer__inner');
+const footerSpans = footer.querySelectorAll('span');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            footerSpans.forEach((span, index) => {
+                setTimeout (() =>{
+                    span.classList.add('animate');
+                }, index * 100); //delay each letter by 100ms
+            });
+            observer.unobserve(footer); //unobserve after animation triggers
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+observer.observe(footer);
